c688b31c39fdff9b6c38bd33b2f85faf81c0a936
balfarot <89477720+balfarot@users.noreply.github.com>
2022-04-30 14:41:51 -0700

    Add files via upload

---
+++ PDF_Reader.py
@@ -0,0 +1,16 @@
+# PDF Reader
+
+import pyttsx3
+import PyPDF2
+
+book = open('mislead.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+speaker = pyttsx3.init()
+
+for num in range(3, pages):
+    page=pdfReader.getPage(num)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
+
